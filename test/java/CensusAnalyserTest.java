d35a6f75a3753d5742003ad9b3eea8e74674341b
priyanka897-star <priyankamore8997@gmail.com>
2021-11-14 01:20:57 +0530

    Added:Functionslity to test code for wrong state file path

--- test/java/CensusAnalyserTest.java
+++ test/java/CensusAnalyserTest.java
@@ -10,7 +10,8 @@ public class CensusAnalyserTest {
         private static final String INDIA_CENSUS_CSV_FILEPATH = "IndiaStateCensusData.csv";
         private static final String WRONG_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
         private static final String INDIA_CENSUS_CSV_WRONGDELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimiter.csv";
-        private static final String STATE_CODE_CSV = "./src/test/resources/IndiaStateCode.csv";
+        private static final String STATE_CODE_CSV = "IndiaStateCode.csv";
+        private static final String WRONG_STATE_CODE_CSV_FILE = "./src/main/resources/IndiaStateCode.csv";
 
         @Test
         public void givenIndiaCensusCsvFile_ReturnsCorrectRecords() {
@@ -47,6 +48,15 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadStateCodeData(STATE_CODE_CSV);
             Assertions.assertEquals(37, numOfRecords);
         }catch (CensusAnalyserException e){ }
+    }
+    public void givenWrongStateCodePath_ReturnsCustomException(){
+        try{
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCodeData(WRONG_STATE_CODE_CSV_FILE);
+            Assertions.assertEquals(37, numOfRecords);
+        }catch (CensusAnalyserException e){
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
     }
 }
 

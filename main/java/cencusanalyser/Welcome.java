edbe76ff6d1194ef60172623ffb7b660e33bff5b
priyanka897-star <priyankamore8997@gmail.com>
2021-11-13 23:12:29 +0530

    Added:Functionality to print welcome messege

---
+++ main/java/cencusanalyser/Welcome.java
@@ -0,0 +1,8 @@
+package cencusanalyser;
+
+public class Welcome {
+        public static void main(String[] args) {
+            System.out.println("Welcome To Census Analyser Problem");
+        }
+}
+

38951b5f1051f49a9dde5accf1ec756f13187843
priyanka897-star <priyankamore8997@gmail.com>
2021-11-13 23:17:54 +0530

    Added:Welcome Messeage

--- main/java/cencusanalyser/Welcome.java
+++ main/java/cencusanalyser/Welcome.java
@@ -2,7 +2,7 @@ package cencusanalyser;
 
 public class Welcome {
         public static void main(String[] args) {
-            System.out.println("Welcome To Census Analyser Problem");
+            System.out.println("Welcome to Cencus Analyser Problems");
         }
 }
 

dcbca2226b7c77ca77273ead11687a6deac06ff7
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-11-14 23:04:58 +0530

    Refactored:Custom Exception for Wrong File path.

--- main/java/cencusanalyser/CensusAnalyserException.java
+++ main/java/cencusanalyser/CensusAnalyserException.java
@@ -2,22 +2,15 @@ package cencusanalyser;
 
     public class CensusAnalyserException extends Exception {
 
-
-         ExceptionType type;
+        public  ExceptionType type;
 
         public enum ExceptionType {
                 CENSUS_FILE_PROBLEM, CSV_FILE_INTERNAL_ISSUES;
             }
 
-
-
-
          public CensusAnalyserException(String message, ExceptionType type) {
                     super(message);
                     this.type = type;
-                }
-
-
-
-    }
+         }
+  }
 

89f0f00e88cbb166191a826ffc2ccf14f5a5a033
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-11-14 23:14:50 +0530

    Refactored:Test Method For Wrong Type.

--- test/java/CensusAnalyserTest.java
+++ test/java/CensusAnalyserTest.java
@@ -2,13 +2,14 @@ import cencusanalyser.CensusAnalyser;
 import cencusanalyser.CensusAnalyserException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
 import java.io.IOException;
 
 
 public class CensusAnalyserTest {
         private static final String INDIA_CENSUS_CSV_FILEPATH = "IndiaStateCensusData.csv";
         private static final String WRONG_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+        private static final String INDIA_CENSUS_WRONGEXTENSION = "./src/test/resources/IndiaStateCensusData.txt";
+        
         @Test
         public void givenIndiaCensusCsvFile_ReturnsCorrectRecords() {
             try {
@@ -23,8 +24,18 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
             Assertions.assertEquals(29, numOfRecords);
-        }catch (CensusAnalyserException | IOException e){
+        }catch (CensusAnalyserException ){
             Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    
+     @Test
+    public void givenWrongType_ReturnsCustomException(){
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_WRONGEXTENSION);
+        }catch (CensusAnalyserException e){
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.WRONG_FILE_EXTENSION, e.type);
         }
     }
 }
